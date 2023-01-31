418797ce2f29ac6580d87430e4798bb58de588b9
elverdavid <elverdavid0839@gmail.com>
2023-01-31 12:00:40 -0500

    new setting

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,5 @@
 import Navbar1 from "./Navbar";
-import Hero from "./componentLandig/Hero";
-import Section from "./componentLandig/oneSection";
+import Hero from "./Hero";
 import { Spacer } from "@nextui-org/react";
 
 const Landing = () => {
@@ -10,7 +9,6 @@ const Landing = () => {
       <div className="lg:mx-16 md:mx-12 mx-7">
         <Hero />
         <Spacer y={5}/>
-        <Section/>
       </div>
     </>
   );
--- src/components/componentLandig/OneSection.jsx
+++
@@ -1,9 +0,0 @@
-const Section = () => {
-  return (
-    <>
-    <h2 className="text-center animate-pulse">incompleto ...</h2> 
-    </>
-  );
-};
-
-export default Section;
